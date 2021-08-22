83c9adad2d3dde5d2962312ff90c51d5ebde40e2
Md. Jabed Hossain <jabed15578@gmail.com>
2021-08-23 01:14:23 +0600

    added solution code of uri problem no. 1002 using c++

---
+++ URI/URI 1002/Area of a Circle.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define pi 3.14159
+using namespace std;
+
+int main()
+{
+    double a, r;
+
+    cin >> r;
+
+    a = pi * r * r;
+
+    cout << "A=" << fixed << setprecision(4) << a << "\n";
+
+    return 0;
+}
