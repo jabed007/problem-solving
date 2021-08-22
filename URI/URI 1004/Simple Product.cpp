e638b0447b40774eeb4c375041c38215a9ef274f
Md. Jabed Hossain <jabed15578@gmail.com>
2021-08-23 01:33:59 +0600

    added solution code of uri problem no. 1004 in c++

---
+++ URI/URI 1004/Simple Product.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a, b, prod;
+
+    cin >> a >> b;
+
+    prod = a * b;
+
+    cout << "PROD = " << prod << "\n";
+
+    return 0;
+}
