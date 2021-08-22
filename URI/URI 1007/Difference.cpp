b61e0f26f1cc0c273962152d677ba43d911a74e6
Md. Jabed Hossain <jabed15578@gmail.com>
2021-08-23 02:09:52 +0600

    added solution code of uri problem no. 1007 in c++

---
+++ URI/URI 1007/Difference.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a, b, c, d, diff;
+
+    cin >> a >> b >> c >> d;
+
+    diff = (a * b - c * d);
+
+    cout << "DIFERENCA = " << diff << "\n";
+
+    return 0;
+}
