d1c8a9bd974f44f6c458f97b701b65e8a7a06a3b
Md. Jabed Hossain <jabed15578@gmail.com>
2021-08-23 00:52:06 +0600

    added solution of uri problem no. 1001

--- URI/URI 1047/Game Time with Minutes.cpp
+++ URI/URI 1047/Game Time with Minutes.cpp
@@ -1,1 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int main()
+{
+    int initial_hour, initial_minute;
+    int final_hour, final_minute;
+
+    cin >> initial_hour >> initial_minute;
+    cin >> final_hour >> final_minute;
+
+    if(initial_hour > 12)
+    {
+        initial_hour = initial_hour - 12;
+    }
+
+    if(final_hour > 12)
+    {
+        final_hour = final_hour - 12;
+    }
+}
