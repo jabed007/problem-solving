6420d66bc26c16b4a1eb1c8a9e29b57bec321d52
Md. Jabed Hossain <jabed15578@gmail.com>
2021-08-24 00:58:06 +0600

    added solution code of uri problem no. 1008 in c++

---
+++ URI/URI 1008/Salary.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    double c;
+
+    cin >> a >> b >> c;
+
+    cout << "NUMBER = " << a << "\n";
+    cout << "SALARY = U$ " << fixed << setprecision(2) << b * c << "\n";
+
+    return 0;
+}
