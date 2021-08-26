e8ae1616d34de13efa218af0ba02ac2192ac2c9a
Md. Jabed Hossain <jabed15578@gmail.com>
2021-08-27 00:48:56 +0600

    added solution code of uri problem no. 1010 in c++

---
+++ URI/URI 1010/Simple Calculate.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a1, b1, a2, b2;
+    double c1, c2;
+
+    cin >> a1 >> b1 >> c1;
+    cin >> a2 >> b2 >> c2;
+
+    cout << "VALOR A PAGAR: R$ " << fixed << setprecision(2) << b1 * c1 + b2 * c2 << "\n";
+
+    return 0;
+}
