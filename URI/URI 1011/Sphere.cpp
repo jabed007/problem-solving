37faf4adc13e7bde4964e656c4bbf21f67a02f85
Md. Jabed Hossain <jabed15578@gmail.com>
2021-08-27 01:01:45 +0600

    added solution code of uri problem no. 1011 in c++

---
+++ URI/URI 1011/Sphere.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define pi 3.14159
+using namespace std;
+
+int main()
+{
+    double r;
+
+    cin >> r;
+
+    cout << "VOLUME = " << fixed << setprecision(3) << 4.0 / 3 * pi * r * r * r << "\n";
+
+    return 0;
+}
