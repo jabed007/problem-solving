319219567093d31afd5ad4a5acfb678cc4e7e8c5
Md. Jabed Hossain <jabed15578@gmail.com>
2021-08-27 00:47:39 +0600

    ädded solution code of codeforces problem 4A in c++

---
+++ Codeforces/4A/A. Watermelon.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int w;
+
+    cin >> w;
+
+    if(w % 2 == 0 && w > 2)
+    {
+        printf("YES\n");
+    }
+    else
+    {
+        printf("NO\n");
+    }
+
+    return 0;
+}
