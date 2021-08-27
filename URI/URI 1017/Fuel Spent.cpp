80e834dae4ba75638e74ac5b3c9fdc68b0c4c163
Md. Jabed Hossain <jabed15578@gmail.com>
2021-08-27 14:33:59 +0600

    added solution code of uri problem no. 1017 in c++

---
+++ URI/URI 1017/Fuel Spent.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int time, speed;
+
+    cin >> time >> speed;
+
+    cout << fixed << setprecision(3) << (time * speed) / 12.0 << "\n";
+
+    return 0;
+}
