8cf10f35532935a7047b7288c6cbd456d3c50051
Md. Jabed Hossain <jabed15578@gmail.com>
2021-08-27 11:36:17 +0600

    added solution code of uri problem no. 1014 in c++

---
+++ URI/URI 1014/Consumption.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int x;
+    double y;
+
+    cin >> x >> y;
+
+    cout << fixed << setprecision(3) << x / y << " km/l" << "\n";
+
+    return 0;
+}
